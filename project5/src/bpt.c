7f4afe0fcc5bee53b4e182972f43fcf021c14a0f
2016025078 <2016025078@hanyang.ac.kr>
2020-12-11 16:29:47 +0900

    test

--- project5/src/bpt.c
+++ project5/src/bpt.c
@@ -476,7 +476,6 @@ pagenum_t nt_find_leaf_page(int table_id, int64_t key)
     // there is no root
     if(header.root_page_num == 0)
     {
-        // printf("there is no root\n");
         return -1;
     }
 

5f76f496957ba2700ac96a1df9d55a4675cfaed8
2016025078 <2016025078@hanyang.ac.kr>
2020-12-12 00:01:59 +0900

    test

--- project5/src/lock_table.c
+++ project5/src/lock_table.c
@@ -420,7 +420,7 @@ int lock_release(lock_t * lock_obj)
 	// if not, nothing to do. predecessor will wake up successor
 	
 
-	// when one transaction can execute one opration(db_find() or db_update()) to one record
+	// // when one transaction can execute one opration(db_find() or db_update()) to one record
 	pred = lock_obj->prev;
 	succ = lock_obj->next;
 
